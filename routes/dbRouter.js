98af03ebf048843d588ccc5330719c6ac14845ae
VasudevanVaisnav <vaisnav21032001@gmail.com>
2021-02-14 16:29:39 +0530

    db router updated

--- routes/dbRouter.js
+++ routes/dbRouter.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const addElement = require('./../handlers/addElement');
+const download = require('./../handlers/download');
+const drop = require('./../handlers/drop');
+const fetchElement = require('./../handlers/fetchElement');
+const updatesCheck = require('./../handlers/updatesCheck');
+const editElement = require('./../handlers/editElement');
 
 router.post('/add',addElement)
 router.post('/edit',editElement)
 router.get('/checkUpdate/:mainToken',updatesCheck)
 router.post('/fetch',fetchElement)
-router.get('/drop/:elementId',drop)
+router.get('/fetchElement/:elementId',drop)
 router.get('/download',download)
 
 module.exports = router;
